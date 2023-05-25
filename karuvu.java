227509400b160b05d58d0b004570d7de092c981c
Siddartha <sidhureddy.legala@gmail.com>
2023-05-25 10:38:44 +0530

    Hi

---
+++ karuvu.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.util.*;
+
+class Sidha{
+    public static void main(String[] args) {
+        System.out.print("hi");
+        System.out.print("hi two");
+        // lambdi kodaka
+    }
+}
